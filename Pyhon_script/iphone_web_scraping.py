da1aff6a804057f31f63ac7e46787c28122158ba
Loveman Singh <lovemansingh10@gmail.com>
2025-03-31 13:16:14 +0530

    2.5

--- Pyhon_script/iphone_web_scraping.py
+++ Pyhon_script/iphone_web_scraping.py
@@ -7,6 +7,8 @@ url = 'https://www.amazon.ae/s?k=iphone&crid=3Q3DZ0PUU1PWJ&sprefix=%2Caps%2C196&
 page = requests.get(url)
 soup = BeautifulSoup(page.text,'html')
 all_listitems = soup.find('div', class_= "s-main-slot s-result-list s-search-results sg-row")
+
+# for gating iPhone modal name from url
 name = all_listitems.find_all('h2')
 clear_name = [_.text for _ in name]
 filtered_list = [item for item in clear_name if item not in ['Results', 'More results','Related searches']]
@@ -17,9 +19,10 @@ filtered_list = [item for item in clear_name if item not in ['Results', 'More re
 
 
 
-#for price
+#for geting price tag from url
 price = all_listitems.find_all('span', class_ = 'a-price-whole')
 clean_price = [_.text for _ in price]
+
 # print(clean_price)
 # print(len(clean_price))
 
@@ -29,6 +32,7 @@ from datetime import datetime
 
 df = pd.DataFrame({'Product Name': filtered_list, 'Price': clean_price, 'date':datetime.today()})
 print(df)
+
 # today
 today_date = str(datetime.today().strftime('%Y-%m-%d'))
 df.to_csv(rf'C:\my_work\IPhone_Price_Everyday\CSV_files\Price_on_{today_date}.csv')
